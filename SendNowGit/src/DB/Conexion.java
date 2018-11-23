08860278bf01b6a9a7b3e8d2fb419a95b2d79bb6
Veneury <deversnolasco@gmail.com>
2018-11-23 17:01:36 -0400

    Primera subida en blanco
    Esta es la primera subida del proyecto, no tiene mas que la estuctura.

---
+++ SendNowGit/src/DB/Conexion.java
@@ -0,0 +1,43 @@
+package DB;
+
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TooDe
+ */
+public class Conexion {
+    
+    private final String base = "sendnow";
+    private final String user = "root";
+    private final String password = "";
+    private final String url = "jdbc:mysql://localhost:3306/" + base;
+    private Connection con = null;
+    
+    public Connection getConexion()
+    {
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+            
+        } catch(SQLException e)
+        {
+            System.err.println(e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      return con;  
+    }
+}
