c277c4ebbb809c9ee99147eca6a640e05c133a60
Veneury <deversnolasco@gmail.com>
2018-11-23 20:14:47 -0400

    Haciendo la segunda teamPrueba

--- SendNowGit/src/Views/Prueba.java
+++ SendNowGit/src/Views/Prueba.java
@@ -10,5 +10,13 @@ package Views;
  * @author TooDe
  */
 public class Prueba {
+   private int num1,num2;
+   
+   private int suma(int num1, int num2)
+   {
+      this.num1=num1;
+      this.num2=num2;
+      return num1 + num2;
+   }
    
 }
